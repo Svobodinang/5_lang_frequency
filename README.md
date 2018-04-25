f521c2af8f0c71ab92558063e9f6ea0913c34795
svobodinang <svobodinang@mail.ru>
2018-04-26 00:33:37 +0300

    first commit

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+This script find ten the most frequency words in your file with text
+
+# How is it work
+
+The script requires the installed Python version 3.5 to work
+
+You can start the method like this:
+```Python
+C:\Users\svobodinang\Desktop\Python\5_lang_frequency>python lang_frequency.py text.txt
+Слово a повторяется 4 раз
+Слово of повторяется 3 раз
+Слово to повторяется 3 раз
+Слово the повторяется 3 раз
+Слово and повторяется 3 раз
+Слово his повторяется 2 раз
+Слово The повторяется 1 раз
+Слово job повторяется 1 раз
+Слово renaissance повторяется 1 раз
+Слово court повторяется 1 раз
+```
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,58 @@
-
+import sys
+import os
 
 def load_data(filepath):
-    pass
+    try:
+        with open(filepath) as text:
+            decoded_text = text.read()
+            paresed_text = decoded_text.split()
+            return paresed_text
+    except IOError:
+        return None
 
 
 def get_most_frequent_words(text):
-    pass
+    word_list = []
+    for word in text:
+        check = False
+        for element in word_list:
+            if word == element[0]:
+                element[1]+=1
+                check = True
+        if check == False:
+            b = [word, 1]
+            word_list.append(b)
+
+    number = 1
+    while number < len(word_list):
+        for index in range(len(word_list) - number):
+            if word_list[index][1] < word_list[index + 1][1]:
+                word_list[index], word_list[index + 1] = word_list[index + 1], word_list[index]
+        number+=1
+
+    if len(word_list) > 10:
+        most_frequent_words = []
+        for number in range(10):
+            most_frequent_words.append(word_list[number])
+    else:
+        most_frequent_words = [0 * len(word_list)]
+        most_frequent_words = word_list
+
+    return most_frequent_words
+
+
+def input_words(most_frequent_words):
+    for number in range(len(most_frequent_words)):
+        print("Слово", most_frequent_words[number][0], "повторяется", most_frequent_words[number][1], "раз")
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) < 2:
+        exit("Вы не ввели путь к файла с данными")
+    file_path = sys.argv[1]
+    if not os.path.isfile(file_path):
+        exit("Такого файла не существует")
+    if load_data(file_path) == None:
+        exit("Проблема с открытием файла")
+
+    input_words(get_most_frequent_words(load_data(file_path)))
